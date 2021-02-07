f2f53b26d7dcb3c37aaec92a29152bfe3cfbfdb0
d-bagazhkov <dmytro.bagazhkov@gmail.com>
2021-02-07 15:48:08 +0200

    add score table

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use crate::piston::EventLoop;
 use std::process;
 use piston::window::WindowSettings;
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{Button, Key, PressEvent, ReleaseEvent, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use glutin_window::GlutinWindow;
-use opengl_graphics::{GlGraphics, OpenGL};
+use opengl_graphics::{GlGraphics, OpenGL, GlyphCache, Filter, TextureSettings};
 
 pub struct App {
-    gl: GlGraphics,
     left_score: i32,
     left_pos: i32,
     left_vel: i32,
@@ -25,11 +25,14 @@ pub struct App {
 }
 
 impl App {
-    fn render(&mut self, args: &RenderArgs) {
+    fn render(&mut self, args: &RenderArgs, gl: &mut GlGraphics, glyph_cache: &mut GlyphCache) {
         use graphics::*;
         
         const BACKGROUND: [f32; 4] = [0.0, 0.5, 0.5, 1.0];
         const FOREGROUND: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+
+        let width = args.window_size[0] as f64;
+        let hight = args.window_size[1] as f64;
 
         let left = rectangle::square(0.0, 0.0, 50.0);
         let left_pos = self.left_pos as f64;
@@ -41,11 +44,17 @@ impl App {
         let ball_x = self.ball_x as f64;
         let ball_y = self.ball_y as f64;
 
-        self.gl.draw(args.viewport(), |c, gl| {
+        let score_text = format!("{}    {}", self.left_score, self.right_score);
+        let score_text_width = glyph_cache.width(32, score_text.as_str()).unwrap();
+
+        gl.draw(args.viewport(), |c, gl| {
             clear(BACKGROUND, gl);
             rectangle(FOREGROUND, left, c.transform.trans(-40.0, left_pos), gl);
-            rectangle(FOREGROUND, right, c.transform.trans(args.window_size[0] as f64 - 10.0, right_pos), gl);
+            rectangle(FOREGROUND, right, c.transform.trans(width - 10.0, right_pos), gl);
             rectangle(FOREGROUND, ball, c.transform.trans(ball_x, ball_y), gl);
+            rectangle(FOREGROUND, ball, c.transform.trans(ball_x, ball_y), gl);
+            line(color::WHITE, 1.0, [width / 2.0, 0.0, width / 2.0, hight], c.transform, gl);
+            text(color::WHITE, 32, score_text.as_str(), glyph_cache, c.transform.trans(width / 2.0 - score_text_width / 2.0, 32.0), gl).unwrap();
         })
     }
 
@@ -136,8 +145,13 @@ fn main() {
             .build()
             .unwrap();
 
+    let mut gl = GlGraphics::new(opengl);
+
+    let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+    let mut glyph_cache = GlyphCache::new("assets/8bitOperatorPlusSC-Regular.ttf", (), texture_settings)
+    .expect("Error unwrapping fonts");
+
     let mut app = App {
-        gl: GlGraphics::new(opengl),
         left_score: 0,
         left_pos: 1,
         left_vel: 0,
@@ -151,9 +165,10 @@ fn main() {
     };
 
     let mut events = Events::new(EventSettings::new());
+    events.set_ups(60);
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
-            app.render(&r);
+            app.render(&r, &mut gl, &mut glyph_cache);
         }
         if let Some(u) = e.update_args() {
             app.update(&u);
